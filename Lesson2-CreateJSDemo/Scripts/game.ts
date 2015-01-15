4ac743e28d7abb77218125e7e73b5657fc2a2227
bhavin0810 <bhavin0810@gmail.com>
2015-01-15 13:43:17 -0500

    Create Js Added

---
+++ Lesson2-CreateJSDemo/Scripts/game.ts
@@ -0,0 +1,24 @@
+﻿var canvas;
+var stage: createjs.Stage;
+var hellotext;
+
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    main();
+}
+function gameLoop() {
+    hellotext.x++;
+    hellotext.y++;
+
+    stage.update();
+}
+
+function main() {
+    hellotext = new createjs.Text("Hello World", "40px Consolas", "#000000");
+
+    stage.addChild(hellotext);
+}
